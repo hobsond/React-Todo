8296dd44fc94c8327237fda889e47c1fbd316b79
unknown <hobsond26@icloud.com>
2020-05-04 21:11:12 -0400

    finished

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Todo(props) {
+    const {item,complete}= props
+
+    return (
+        <div >
+        <h2 onClick={complete} name={item.todo}> {item.todo} </h2>
+
+            
+        </div>
+    )
+}
---
+++ src/components/TodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TodoForm(props) {
+    const {inputChange,submitChange,newtodo,clear} = props
+    return (
+        <form onSubmit={submitChange} >
+            <input name='todo'value={newtodo.todo} onChange={inputChange} type='text' />
+            <button onClick={submitChange}> submit </button>
+            <button onClick={clear}> Clear Completed </button>
+            
+        </form>
+    )
+}
